faa638feaaa41e8a13562bf44f966fd3c71a6409
Sheena Galutira <Sheena@Sheenas-MacBook-Pro.local>
2016-05-03 01:04:16 -1000

    Completed basics.js

--- basics.js
+++ basics.js
@@ -7,20 +7,41 @@ var myName = "sheena";
  * as well as an `age` (number);
  */
 
+ var person = {name: "sheena", age: 30};
+
 /* Create a variable called `canDrive`,
  * if it should be true if your person object is at least 16 years old
  */
+
+if (person.age >= 16){
+	var canDrive = true;
+}
 
 /* Create a function called `greet`,
  * it should take a 1 parameter, `name`
  * and it should print "Hello, my name is {name}"
  */
+ function greet(name){
+ 	console.log("Hello, my name is " + name);
+ }
+ greet("sheena");
 
 /* Create an array called `dataTypes` with atleast 1 of every data type;
  * (there are 6 different data types);
  */
+ var dataTypes = ['string', 10, true, {food:"taco", drink:"water"}, ['red', 'green','blue'], null, undefined];
 
 /* Create a `dog` object
  * it should have a `bark` function that makes your dog bark!
  * It should also have a name attribute with the value of 'Spot'
  */
+ var dog = {
+ 	name:"Spot",
+ 	bark: function(){
+ 		console.log("woof woof");
+ 	}
+};
+
+dog.bark();
+
+
